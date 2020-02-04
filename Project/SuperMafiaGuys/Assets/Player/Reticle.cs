dc9fbc3cf3a86849d5dd0bd0dc5c705ce5953bfe
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2020-02-04 07:43:56 -0800

    Cell shading added proper

--- Project/SuperMafiaGuys/Assets/Player/Reticle.cs
+++ Project/SuperMafiaGuys/Assets/Player/Reticle.cs
@@ -6,13 +6,30 @@ namespace Player
 {
     public class Reticle : MonoBehaviour
     {
-        public UnityEngine.UI.Image image;
+        public UnityEngine.UI.Graphic[] graphics;
 
         public static bool show = false;
 
+        private bool graphicsEnabled
+        {
+            get
+            {
+                return graphics[0].enabled;
+            }
+            set
+            {
+                for (int i = 0; i < graphics.Length; i++)
+                {
+                    var item = graphics[i];
+
+                    item.enabled = value;
+                }
+            }
+        }
+
         private void Update()
         {
-            image.enabled = show;
+            graphicsEnabled = show;
         }
     }
 }
